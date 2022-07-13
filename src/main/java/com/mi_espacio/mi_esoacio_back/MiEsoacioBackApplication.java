c9a504bd6af05d6393b47e5b1a5ff42b17abb23d
ARAMOSJ1\ariel <arielbryan1@hotmail.com>
2022-07-13 10:38:47 -0500

    Estructura del proyecto JAVA Spring boot

---
+++ src/main/java/com/mi_espacio/mi_esoacio_back/MiEsoacioBackApplication.java
@@ -0,0 +1,13 @@
+package com.mi_espacio.mi_esoacio_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiEsoacioBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiEsoacioBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mi_espacio/mi_esoacio_back/MiEsoacioBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mi_espacio.mi_esoacio_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiEsoacioBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
